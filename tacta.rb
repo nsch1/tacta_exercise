8bdad160d1949cda899c4c1b67acb188c9f9b476
nsch1 <x>
2018-02-16 14:17:48 +0100

    Read/Write in seperate file

--- tacta.rb
+++ tacta.rb
@@ -1,16 +1,5 @@
 require "json"
-
-def read_contacts
-	json = File.read("contacts.json")
-	array = JSON.parse(json, { symbolize_names: true })
-end
-
-def write_contacts(contacts)
-	File.open("contacts.json", "w") do |file|
-		json = JSON.pretty_generate(contacts)
-		file.write(json)
-	end
-end
+require "./contacts_file"
 
 def index(contacts)
 	contacts.each_with_index do |contact, index|
